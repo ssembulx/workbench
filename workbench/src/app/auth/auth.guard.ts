678d8240a8ac595145323ea7eeef2e6b817b2c39
ssembulx <ssembulx@SSEMBULX-MOBL>
2023-02-21 17:52:42 +0530

    changes

--- workbench/src/app/auth/auth.guard.ts
+++ workbench/src/app/auth/auth.guard.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Router, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  Router,
+  CanActivate,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { SummaryService } from '../shared/service';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-
   userDetails: any;
 
   headers = new HttpHeaders({
@@ -18,14 +22,11 @@ export class AuthGuard implements CanActivate {
     'Access-Control-Allow-Headers': 'Content-Type',
     'Access-Control-Allow-Methods': 'GET,POST,OPTIONS,DELETE,PUT',
   });
-  constructor(private service: SummaryService, private router: Router) { }
+  constructor(private service: SummaryService, private router: Router) {}
 
-  async canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot) {
-
+  async canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     // ***** Local code ***** //
-    const resAuth: any = {
+    /*     const resAuth: any = {
         "emailId": "arundathix.manjunath@intel.com",
         "name": "Manjunath, ArundathiX",
         "idsid": "arundatx",
@@ -48,28 +49,32 @@ export class AuthGuard implements CanActivate {
         debugger
       this.router.navigate(['/access-ristrict']);
       return false
-    }
+    } */
 
     // ******  Server code ****** //
-      // const tokenData: any = await this.service.getWindowsAuthP()
-      // const resAuth: any = await this.service.getUserDetailP({ token: tokenData.token })
-      // console.log(resAuth,"res Auth")
-      // if (resAuth.Role == 'Admin' || resAuth.Role == 'User' || resAuth.Role == 'Manager') {
-      //   this.service.setValue(true);
-      //   return true
-      // } else {
-      //   this.router.navigate(['/access-ristrict']);
-      //   return false
-      // }
+    const tokenData: any = await this.service.getWindowsAuthP();
+    const resAuth: any = await this.service.getUserDetailP({
+      token: tokenData.token,
+    });
+    console.log(resAuth, 'res Auth');
+    if (
+      resAuth.Role == 'Admin' ||
+      resAuth.Role == 'User' ||
+      resAuth.Role == 'Manager'
+    ) {
+      this.service.setValue(true);
+      return true;
+    } else {
+      this.router.navigate(['/access-ristrict']);
+      return false;
+    }
 
-
-
-    //   if (!this.helper.token) {   
+    //   if (!this.helper.token) {
     //     let userToken:any = await this.service.getWindowsAuthP();
     //     const payload = { "userToken": userToken.token }
     //     const userinfo: any = await this.service.getUserDetailP(payload)
     //      this.userDetails = userinfo
-    //      this.helper.authenticationDone(true)  
+    //      this.helper.authenticationDone(true)
     //      if (this.userDetails.isAuthorize) {
     //       this.helper.setToken(this.userDetails.token)
     //       const data : any=   await this.service.getUserData()
@@ -97,16 +102,15 @@ export class AuthGuard implements CanActivate {
     //      Object.assign(userInfo ,{userConfig:data.userConfig})
     //        this.helper.editUser(userInfo);
     //        this.helper.SetUser(userInfo);
-    //       //  this.accountService.setShowHeader(true);          
+    //       //  this.accountService.setShowHeader(true);
     //        return true;
     //       } else {
     //       // this.accountService.setShowHeader(false);
     //       this.router.navigate(['/access-ristrict']);
     //       return false
-    //      } 
+    //      }
     //  } else {
     //    return true
     //  }
-    }
-
   }
+}
