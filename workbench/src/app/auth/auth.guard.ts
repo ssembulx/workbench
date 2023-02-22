adb07eaaad9304450ed99e2c19de46a87bb27964
Manjunath, ArundathiX <arundathix.manjunath@intel.com>
2023-02-22 10:15:02 +0530

    modal popup style changes done

--- workbench/src/app/auth/auth.guard.ts
+++ workbench/src/app/auth/auth.guard.ts
@@ -26,48 +26,48 @@ export class AuthGuard implements CanActivate {
 
   async canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     // ***** Local code ***** //
-    //     const resAuth: any = {
-    //     "emailId": "arundathix.manjunath@intel.com",
-    //     "name": "Manjunath, ArundathiX",
-    //     "idsid": "arundatx",
-    //     "wwid": 12035082,
-    //     "employeeBadgeType": "GB",
-    //     "avatarURL": "https://photos.intel.com/images/12035082.jpg",
-    //     "role": null,
-    //     "domain": null,
-    //     "comments": null,
-    //     "displayName": "Manjunath, ArundathiX",
-    //     "isApplicationAccess": false,
-    //     "programAccesses": null,
-    //     "Role": "Admin"
-    // }
-    // if (resAuth.Role == 'Admin' || resAuth.Role == 'User' || resAuth.Role == 'Manager') {
-    //     debugger
-    //   this.service.setValue(true);
-    //   return true
-    // } else {
-    //     debugger
-    //   this.router.navigate(['/access-ristrict']);
-    //   return false
-    // } 
+        const resAuth: any = {
+        "emailId": "arundathix.manjunath@intel.com",
+        "name": "Manjunath, ArundathiX",
+        "idsid": "arundatx",
+        "wwid": 12035082,
+        "employeeBadgeType": "GB",
+        "avatarURL": "https://photos.intel.com/images/12035082.jpg",
+        "role": null,
+        "domain": null,
+        "comments": null,
+        "displayName": "Manjunath, ArundathiX",
+        "isApplicationAccess": false,
+        "programAccesses": null,
+        "Role": "Admin"
+    }
+    if (resAuth.Role == 'Admin' || resAuth.Role == 'User' || resAuth.Role == 'Manager') {
+        debugger
+      this.service.setValue(true);
+      return true
+    } else {
+        debugger
+      this.router.navigate(['/access-ristrict']);
+      return false
+    } 
 
     // ******  Server code ****** //
-    const tokenData: any = await this.service.getWindowsAuthP();
-    const resAuth: any = await this.service.getUserDetailP({
-      token: tokenData.token,
-    });
-    console.log(resAuth, 'res Auth');
-    if (
-      resAuth.Role == 'Admin' ||
-      resAuth.Role == 'User' ||
-      resAuth.Role == 'Manager'
-    ) {
-      this.service.setValue(true);
-      return true;
-    } else {
-      this.router.navigate(['/access-ristrict']);
-      return false;
-    }
+    // const tokenData: any = await this.service.getWindowsAuthP();
+    // const resAuth: any = await this.service.getUserDetailP({
+    //   token: tokenData.token,
+    // });
+    // console.log(resAuth, 'res Auth');
+    // if (
+    //   resAuth.Role == 'Admin' ||
+    //   resAuth.Role == 'User' ||
+    //   resAuth.Role == 'Manager'
+    // ) {
+    //   this.service.setValue(true);
+    //   return true;
+    // } else {
+    //   this.router.navigate(['/access-ristrict']);
+    //   return false;
+    // }
 
     //   if (!this.helper.token) {
     //     let userToken:any = await this.service.getWindowsAuthP();
