42c7a97deff59f4568042ced02459f0699691217
ssembulx <ssembulx@SSEMBULX-MOBL>
2023-03-17 18:02:36 +0530

    changes

--- workbench/src/app/approve/approve.component.ts
+++ workbench/src/app/approve/approve.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  ChangeDetectorRef,
+} from '@angular/core';
 import { SummaryService } from '../shared/service';
 import { ToastrService } from 'ngx-toastr';
 import { NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
@@ -15,7 +21,8 @@ export class ApproveComponent implements OnInit {
     private dataSvc: SummaryService,
     private toastrService: ToastrService,
     private modalService: NgbModal,
-    config: NgbModalConfig
+    config: NgbModalConfig,
+    private cd: ChangeDetectorRef
   ) {
     config.backdrop = 'static';
     config.size = 'md';
@@ -182,10 +189,13 @@ export class ApproveComponent implements OnInit {
   isCheckedAll = false;
   checkAll(event: any) {
     if (event.currentTarget.checked == true) {
-      this.isChecked = true;
+      this.isChecked = false;
+      this.cd.detectChanges();
+      this.approveBenchList = [];
       this.approvallist.forEach((element: any) => {
         this.approveBenchList.push(element.id);
       });
+      this.isChecked = true;
     } else if (event.currentTarget.checked == false) {
       this.isChecked = false;
       this.approveBenchList = [];
